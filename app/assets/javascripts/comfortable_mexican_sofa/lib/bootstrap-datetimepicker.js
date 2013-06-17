815f33dfc2548f0cbfb89b23744b39229f54f39d
Rimas Silkaitis <neovintage@gmail.com>
2013-06-17 14:22:07 -0700

    change name cause utf8

--- app/assets/javascripts/comfortable_mexican_sofa/lib/bootstrap-datetimepicker.js
+++ app/assets/javascripts/comfortable_mexican_sofa/lib/bootstrap-datetimepicker.js
@@ -3,7 +3,7 @@
  * =========================================================
  * Copyright 2012 Stefan Petre
  * Improvements by Andrew Rowls
- * Improvements by Sébastien Malot
+ * Improvements by Sbastien Malot
  * Project URL : http://www.malot.fr/bootstrap-datetimepicker
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
